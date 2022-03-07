3b8ab687542fd84e36e36c376d88e3e6505cbb81
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2022-03-07 22:57:51 +0100

    Add project files.

---
+++ ZseTimetable/Controllers/OtimetableController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ZseTimetable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OtimetableController : ControllerBase
+    {
+        private readonly ILogger<OtimetableController> _logger;
+        private HttpClient _client;
+
+        public OtimetableController(ILogger<OtimetableController> logger)
+        {
+            _logger = logger;
+            _client = new HttpClient();
+        }
+
+        [HttpGet]
+        public async Task<string> GetAsync(int id = 1)
+        {
+            var response = await _client.GetStringAsync("https://plan.zse.bydgoszcz.pl/plany/o"+id+ ".html");
+            return response;
+        }
+    }
+}

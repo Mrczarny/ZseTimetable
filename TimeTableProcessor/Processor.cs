ce0239cb87f26431802c6dbff254412c5d904007
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2022-03-14 23:04:18 +0100

    Added processors

---
+++ TimeTableProcessor/Processor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TimetableLib.Changes;
+
+namespace TimeTableProcessor
+{
+    public class Processor
+    {
+        private HttpClient _client;
+        public Processor(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<TeacherChange>> ChangesProcessor()
+        {
+        
+        }
+        public async Task<List<ClassChange>> TimeetableProcessor()
+        {
+
+        }
+    }
+}

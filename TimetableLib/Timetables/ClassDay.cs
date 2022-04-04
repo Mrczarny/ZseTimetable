3e17bba64de4d8013a70d0e700c5563eacbed3d4
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2022-04-04 17:24:22 +0200

    Timetables models slightly changed

--- TimetableLib/Timetables/ClassDay.cs
+++ TimetableLib/Timetables/ClassDay.cs
@@ -6,7 +6,6 @@ namespace TimetableLib.Timetables
     public class ClassDay
     {
         public long Id { get; set; }
-        public Class Class { get; set; }
         public DayOfWeek DayOfWeek { get; set; }
         public List<ClassLesson> Lessons { get; set; }
     }
--- TimetableLib/Timetables/ClassTimetable.cs
+++ TimetableLib/Timetables/ClassTimetable.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TimetableLib.Timetables
 {
     public class ClassTimetable
     {
         public long Id { get; set; }
+        public Class Class { get; set; }
         public List<ClassDay> DaysTimetable { get; set; } // dumb name
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
     }
 }

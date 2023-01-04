ff074a5d261ec9631dc4b2ec2b44c0a9ae649a23
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-01-05 00:09:08 +0100

    TimetablesAccess added

---
+++ TimetableLib/DataAccess/TimetablesAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimetableLib.DataAccess;
+using TimetableLib.Models.DBModels;
+
+namespace TimetableLib.DBAccess
+{
+    public abstract class TimetablesAccess : DbAccess
+    {
+        //Get by Name
+        public abstract T? GetByName<T>(string name) where T : class, ITimetables, new(); // TODO - add restraint to this
+
+        //Get by Name
+        public abstract T? GetByLink<T>(string name) where T : class, ITimetables, new();
+    }
+}

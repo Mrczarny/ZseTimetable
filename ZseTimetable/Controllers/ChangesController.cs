737faadbd6ea805c8b23afb5dbba3b15fd3e2a50
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-01-15 23:22:32 +0100

    ChangesService added, changesController modified due to that

--- ZseTimetable/Controllers/ChangesController.cs
+++ ZseTimetable/Controllers/ChangesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -9,7 +10,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using TimetableLib.DataAccess;
+using TimetableLib.DBAccess;
 using TimetableLib.Models.DBModels;
+using TimetableLib.Models.DTOs;
 using TimetableLib.Models.Replacements;
 using TimetableLib.Models.ScrapperModels;
 
@@ -21,48 +24,43 @@ namespace ZseTimetable.Controllers
     {
         private readonly ILogger<ChangesController> _logger;
         private readonly HttpClient _client;
-        private DateTime _lastScrap;
-        //private DataAccess _db;
-        private readonly ChangesScrapper _scrapper;
+        private ChangesAccess _db;
+        //private readonly ChangesScrapper _scrapper;
 
-        public ChangesController(IConfiguration config, ILogger<ChangesController> logger, HttpClient client)
+        public ChangesController(IConfiguration config, ILogger<ChangesController> logger, IHttpClientFactory client,
+            IDataWrapper db)
         {
             _logger = logger;
-            _client = client;
-            //_db = db;
-            _scrapper = new ChangesScrapper(config.GetSection(ScrapperOption.Position)
-                .GetSection("Changes")
-                .GetChildren().Select(x => x.Get<ScrapperOption>())
-            );
+            _client = client.CreateClient("baseHttp");
+            _db = db.ChangesAccess;
+            //_scrapper = new ChangesScrapper(config.GetSection(ScrapperOption.Position)
+            //    .GetSection("Changes")
+            //    .GetChildren().Select(x => x.Get<ScrapperOption>())
+            //);
         }
 
 
-        [HttpGet]
-        [Produces(MediaTypeNames.Application.Json)]
-        public async Task<ActionResult<DayReplacements>> GetChangesAsync()
-        {
-            if (DateTime.Now > _lastScrap.AddMinutes(3))
-            {
-                try
-                {
-                    var rawChanges = await _client.GetStreamAsync("https://localhost:5005/MockServer/zmiany");
-                    _lastScrap = DateTime.Now;
-                    var jsonChanges = _scrapper.Scrap(await new StreamReader(rawChanges).ReadToEndAsync());
-                    rawChanges.Close();
-                    //_db.Update<ReplacementDB>(jsonChanges.Replacements);
-                    return jsonChanges;
+        //[HttpGet]
+        //[Produces(MediaTypeNames.Application.Json)]
+        //public async Task<ActionResult<IEnumerable<ReplacementDTO>>> GetChangesAsync()
+        //{
+        //    try
+        //    {
+        //        DayReplacements scrappedChanges;
+        //        using (var rawChanges = await _client.GetStreamAsync("zmiany"))
+        //        {
+        //            scrappedChanges = _scrapper.Scrap(await new StreamReader(rawChanges).ReadToEndAsync());
+        //        }
 
-                }
-                catch (HttpRequestException exception)
-                {
-                    return Problem(exception.Message);
-                }
-            }
-            else
-            {
-                throw new Exception("Too fast");
-                //return _db.Get<DayReplacements>();
-            }
-        }
+        //        //_db.Update<ReplacementDB>(jsonChanges.Replacements);
+        //        throw new NotImplementedException();
+
+        //    }
+        //    catch (HttpRequestException exception)
+        //    {
+        //        return Problem(exception.Message);
+        //    }
+        //}
     }
 }
+
---
+++ ZseTimetable/Services/ChangesService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TimetableLib.DataAccess;
+using TimetableLib.Models.DBModels;
+using TimetableLib.Models.ScrapperModels;
+
+namespace ZseTimetable.Services
+{
+    public class ChangesService : IHostedService, IDisposable
+    {
+        private readonly ILogger<ChangesService> _logger;
+        private Timer? _timer = null;
+        private ChangesAccess _db;
+        private IEnumerable<TimetableServiceOption> TimetablesTypes;
+        private ChangesScrapper _scrapper;
+        private IConfiguration _config;
+        private HttpClient _client;
+
+        public ChangesService(ILogger<ChangesService> logger, IDataWrapper db, IConfiguration config, IHttpClientFactory client)
+        {
+            _logger = logger;
+            _db = db.ChangesAccess;
+            _config = config;
+            _client = client.CreateClient("baseHttp");
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Changes Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromMinutes(3));
+
+            return Task.CompletedTask;
+        }
+
+        private async void DoWork(object? state)
+        {
+            await using (_scrapper = new ChangesScrapper(_config.GetSection(ScrapperOption.Position)
+                             .GetSection("Changes")
+                             .GetChildren().Select(x => x.Get<ScrapperOption>())))
+            {
+                await DatabaseUpload(_db.GetDBModels<ReplacementDB>(GetAllReplacements()));
+            }
+        }
+
+        private async Task DatabaseUpload(IAsyncEnumerable<ReplacementDB> DbModels)
+        {
+            await foreach (var dbModel in DbModels)
+            {
+                var records = _db.GetByDate<ReplacementDB>(DateTime.Today);
+                if (records != null)
+                {
+                    CreateMissing(records, dbModel);
+                }
+                else
+                {
+                    _db.Create(dbModel);
+                }
+            }
+        }
+
+        private void CreateMissing(IEnumerable<ReplacementDB> records, ReplacementDB dbModel)
+        {
+
+            foreach (var record in records)
+            {
+                if (record.LessonId == dbModel.LessonId &&  
+                    record.TeacherId == dbModel.TeacherId &&
+                    record.ClassroomId == dbModel.ClassroomId &&
+                    record.LessonNumber == dbModel.LessonNumber)
+                {
+                    return;
+                }
+            }
+
+            _db.Create(dbModel);
+        }
+
+        private async IAsyncEnumerable<IPersist> GetAllReplacements()
+        {
+            using (var rawChanges = await _client.GetStreamAsync($"https://zastepstwa.zse.bydgoszcz.pl"))
+            {
+                var spChanges = _scrapper.Scrap(await new StreamReader(rawChanges).ReadToEndAsync());
+                foreach (var tReplacement in spChanges.Replacements)
+                {
+                    foreach (var lReplacement in tReplacement.ClassReplacements)
+                    {
+                        yield return lReplacement;
+                    }
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Changes Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}

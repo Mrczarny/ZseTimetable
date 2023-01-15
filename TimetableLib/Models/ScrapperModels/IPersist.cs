489db3dc02c5ed18684223e681e50b650ccd5d67
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-01-15 23:23:08 +0100

    IPersis added

---
+++ TimetableLib/Models/ScrapperModels/IPersist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimetableLib.Models.DBModels;
+
+namespace TimetableLib.Models.ScrapperModels
+{
+    public interface IPersist
+    {
+        public T GetDBModel<T>() where T : class, IDBModel;
+    }
+}
--- TimetableLib/Models/ScrapperModels/Replacements/LessonReplacement.cs
+++ TimetableLib/Models/ScrapperModels/Replacements/LessonReplacement.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using TimetableLib.Models.DBModels;
+using TimetableLib.Models.ScrapperModels;
 
 namespace TimetableLib.Changes
 {
-    public class LessonReplacement
+    public class LessonReplacement : IPersist
     {
-        public string LessonNumber { get; set; }
+        public string TeacherName { get; set; }
+        public byte LessonNumber { get; set; }
         public string Description { get; set; }
         public string Sub { get; set; }
         public string Note { get; set; }
+
+        public T GetDBModel<T>() where T : class, IDBModel
+        {
+            return new ReplacementDB(this) as T;
+        }
     }
 }

c2546aaffaa2f8090445e290171f49664903a084
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-05-10 02:18:09 +0200

    added new ctor and ClassName property to ReplacementDTO

--- TimetableLib/Models/DTOs/ReplacementDTO.cs
+++ TimetableLib/Models/DTOs/ReplacementDTO.cs
@@ -12,12 +12,22 @@ namespace TimetableLib.Models.DTOs
             Lesson = new LessonDTO(ls);
             NewClassroomName = rp.ClassroomName;
             NewTeacherName = rp.SubTeacherName;
+            ClassName = rp.ClassName;
+        }
+        public ReplacementDTO(ReplacementDB rp)
+        {
+            // Id = (long) rp.Id;
+            Note = rp.Note;
+            NewClassroomName = rp.ClassroomName;
+            NewTeacherName = rp.SubTeacherName;
+            ClassName = rp.ClassName;
         }
 
         //public long Id { get; set; }
         public DateTime ReplacementDate { get; set; }
         public LessonDTO Lesson { get; set; }
         public string NewTeacherName { get; set; }
+        public string ClassName { get; set; }
         //public long NewTeacherId { get; set; }
         public string NewClassroomName { get; set; }
         //public long NewClassroomId { get; set; }
